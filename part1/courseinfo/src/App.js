f9be11df6e245304451867e50225e07f2cbbe1f4
cherylfong <42821602+cherylfong@users.noreply.github.com>
2023-07-10 15:31:00 -0700

    refractor variables in the App component as an entire object for section 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -9,30 +9,36 @@
  * 
  * 1.4 Place the objects into an array.
  * 
+ * 1.5 Change the course and its parts into a single JavaScript object.
+ * 
  */
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
