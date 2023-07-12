25ee4c27436554a594a12b19c60ef1cb4029bd66
cherylfong <42821602+cherylfong@users.noreply.github.com>
2023-07-11 18:50:15 -0700

    complete section 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,7 @@
 /**
  * 1.6 Implement a web application for collecting customer feedback. There are only three options for feedback: good, neutral, and bad.
+ * 
+ * 1.7 Show statistics that include total number of collected feedback, average score where (good:1, neutral:0, bad:-1), and the percentage of good feedback.
  */
 
 import { useState } from 'react'
@@ -9,24 +11,37 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
+  // updates and sets the number of "good" button clicks
+  // updates total of all button clicks
   const handleGoodClick = () => {
     const updateGood = good + 1
     setGood(updateGood)
+    setTotal(updateGood + neutral + bad)
   }
 
+  // updates and sets the number of "neutral" button clicks
+  // updates total of all button clicks
   const handleNeutralClick = () => {
     const updateNeutral = neutral + 1
     setNeutral(updateNeutral)
-
+    setTotal(updateNeutral + good + bad)
   }
 
+  // updates and sets the number of "bad" button clicks
+  // updates total of all button clicks
   const handleBadClick = () => {
     const updateBad = bad + 1
     setBad(updateBad)
-    // setTotal(updateBad + bad)
+    setTotal(updateBad + good + neutral)
   }
+  
+  // gets the average of all feedback
+  const getAverage = (good, bad, total) => { return (good - bad) / total }
 
+  // gets the percentage of good feedback
+  const getGoodPercentage = (good, total) => { return (good / total) * 100 }
 
   return (
     <div>
@@ -38,7 +53,9 @@ const App = () => {
       <P text={"good"} value={good}/>
       <P text={"neutral"} value={neutral}/>
       <P text={"bad"} value={bad}/>
-
+      <P text={"all"} value={total} />
+      <P text={"average"} value={ getAverage(good, bad, total) } />
+      <P text={"positive in %"} value={ getGoodPercentage(good, total)} />
     </div>
   )
 }
@@ -52,6 +69,5 @@ const Button = ({handle, text}) => <button onClick={handle}>{text}</button>
  * Display values
  */
 const P = ({text, value}) => <p>{text} {value}</p>
-
 
 export default App
