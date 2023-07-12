8fbae52591709a07a0515f2aa86788c7f0a86bcc
cherylfong <42821602+cherylfong@users.noreply.github.com>
2023-07-11 18:29:55 -0700

    complete section 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,3 +1,7 @@
+/**
+ * 1.6 Implement a web application for collecting customer feedback. There are only three options for feedback: good, neutral, and bad.
+ */
+
 import { useState } from 'react'
 
 const App = () => {
@@ -6,11 +10,48 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => {
+    const updateGood = good + 1
+    setGood(updateGood)
+  }
+
+  const handleNeutralClick = () => {
+    const updateNeutral = neutral + 1
+    setNeutral(updateNeutral)
+
+  }
+
+  const handleBadClick = () => {
+    const updateBad = bad + 1
+    setBad(updateBad)
+    // setTotal(updateBad + bad)
+  }
+
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <Button handle={handleGoodClick} text={"good 😃"} />
+      <Button handle={handleNeutralClick} text={"neutral 😐"} />
+      <Button handle={handleBadClick} text={"bad 😟"} />
+      <h1>statistics</h1>
+      <P text={"good"} value={good}/>
+      <P text={"neutral"} value={neutral}/>
+      <P text={"bad"} value={bad}/>
+
     </div>
   )
 }
+
+/**
+ * Custom Button
+ */
+const Button = ({handle, text}) => <button onClick={handle}>{text}</button>
+
+/**
+ * Display values
+ */
+const P = ({text, value}) => <p>{text} {value}</p>
+
 
 export default App
