d948ad56cd85537cd75cddef6d121facfac4a9ca
cherylfong <42821602+cherylfong@users.noreply.github.com>
2023-07-13 13:23:23 -0700

    add examples from Part 1 section a

--- part2/preliminary/src/App.js
+++ part2/preliminary/src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = (props) => {
+  
+  // props in the App parameter is from index.js
+  // 
+  // this is a destructuring syntax
+  // similar to:
+  // const notes = props.notes
+  const { notes } = props
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Notes</h1>
+      
+      {/* Alternatives for the same component */}
+      
+      {/* <ul>
+        {notes.map(note =>
+          <li>
+            key={note.content}
+          </li>
+        )} */}
+
+      {/* 
+        <ul>
+         {notes.map( note =>
+            <li key={note.id}>
+              {note.content}
+            </li>
+         )}
+      
+      */}
+
+      {/* <ul>
+        {notes.map((note, i) =>
+          <li key={i}>
+            {note.content}
+          </li>
+        )} 
+      */}
+
+      <ul>
+        { notes.map(note =>
+          <Note key={note.id} note={note}/>
+        )}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+const Note = ({note}) => {
+  return(
+    <li>{note.content}</li>
+  )
+}
+
+export default App
