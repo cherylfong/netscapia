da40a260f83820357163c0640fecef3d7ba85277
cherylfong <42821602+cherylfong@users.noreply.github.com>
2023-07-19 13:23:48 -0700

    complete section 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -9,12 +9,15 @@
  * 
  * 2.10 Refractor components into separate component files and place into ./component .
  * 
+ * 2.11 Add db.json file to project. Modify the application such that the initial state of the data is fetched from the server using the axios-library. Complete the fetching with an Effect hook.
+ * 
  */
 
 import logo from './logo.svg';
 import './App.css';
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Entry from "./components/Entry"
 import People from "./components/People"
@@ -22,13 +25,27 @@ import Filter from "./components/Filter"
 
 function App(props) {
 
-  const [persons, setPersons] = useState(props.persons) 
+  // no longer retrieving data from index.js
+  // const [persons, setPersons] = useState(props.persons) 
+
+  const [persons, setPersons] = useState([]) 
   
   const [newName, setNewName] = useState('Add Name Here')
 
   const [newNumber, setNewNumber] = useState('Add Number Here')
 
   const [newSearch, setNewSearch] = useState("")
+
+
+  useEffect(() => {
+    console.log("Use effect")
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   return (
     <div>
