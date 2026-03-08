1b241f16c2a70859a4e5d6c13229a11e16189dda
cherylfong <42821602+cherylfong@users.noreply.github.com>
2026-03-08 08:33:19 +0000

    complete exercise 3.1
    Create a simple API route to return a GET request for an array called persons.

---
+++ part3/phonebook/index.js
@@ -0,0 +1,37 @@
+// TODO 3.1: allow starting app using `npm start` and `npm run dev` commands.
+// DONE - defined in package.json.
+
+// TODO 3.1: use provided json data and create /api/persons endpoint
+const express = require('express');
+const app = express();
+const PORT = 3001;
+
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
+
+let persons = [
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+];
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons);
+});
