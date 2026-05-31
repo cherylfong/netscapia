7b08ee7b0b227ce30de6cbb6cb68b74ac501c5f7
cherylfong <42821602+cherylfong@users.noreply.github.com>
2026-05-31 14:50:49 +0000

    add useState to store notes in App component's state

--- part2-2026/notes-2026/src/App.jsx
+++ part2-2026/notes-2026/src/App.jsx
@@ -1,14 +1,23 @@
+// to save notes in App component's state
+import { useState } from 'react'
+
 import Note from './components/Note'
 
-const App = ({ notes }) => {
 
-  console.log(notes)
+// remove props param and replace props.notes with [] empty array 
+// if want to start with not using notes array from
+// main.jsx
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+
+  console.log(notes) // notes as an array extracted from note object
+  console.log(props) // note object from main.jsx
 
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => 
+        {notes.map(note =>
           <Note key={note.id} note={note} />
         )}
       </ul>
