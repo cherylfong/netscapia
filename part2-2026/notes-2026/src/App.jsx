8ac8f1ccd26799065d264465e34b8f9a0d4a2b49
cherylfong <42821602+cherylfong@users.noreply.github.com>
2026-05-31 15:35:18 +0000

    add form field with submission suspended and event handlers
    Event handlers include button clicks and input field text changes.

--- part2-2026/notes-2026/src/App.jsx
+++ part2-2026/notes-2026/src/App.jsx
@@ -1,4 +1,4 @@
-// to save notes in App component's state
+// to save component states
 import { useState } from 'react'
 
 import Note from './components/Note'
@@ -10,8 +10,35 @@ import Note from './components/Note'
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
 
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  )
+
   console.log(notes) // notes as an array extracted from note object
   console.log(props) // note object from main.jsx
+
+  // this event handler is called when the form is submitted
+  const addNote = (event) => {
+    event.preventDefault() // prevents form submission
+
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1),
+    }
+
+    setNotes(notes.concat(noteObject)) // THIS DOES NOT MUTATE ORIGINAL notes ARRAY -- append new object to notes
+    setNewNote('') // clear input field
+
+    console.log('button clicked !!!', event.target)
+  }
+
+  // To enable editing of the input element, register an event handler that synchronizes the changes made to the input with the component's state:
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
 
   return (
     <div>
@@ -21,6 +48,10 @@ const App = (props) => {
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form>
    </div>
   )
 }
