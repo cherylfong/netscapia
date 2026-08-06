a4310cde74817cca059b96421f12127a9604605f
cherylfong <42821602+cherylfong@users.noreply.github.com>
2026-08-06 13:08:27 +0000

    complete exercise 2.8
    Added a form field for numbers to be stored with names in the person
    object array.
    Handles typing within the field box and allows adding to the array.

--- part2-2026/phonebook-2026/src/App.jsx
+++ part2-2026/phonebook-2026/src/App.jsx
@@ -5,15 +5,19 @@ import Person from './components/Person'
 const App = () => {
 
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas', number: '040-1234567'
+         }
     ])
 
     const [newName, setNewName] = useState('')
 
-    const addName = (event) => {
+    const [newNumber, setNewNumber] = useState('')
+
+    const addEntry = (event) => {
         event.preventDefault() // prevents form submission
 
         const trimmedName = newName.trim()
+        const trimmedNumber = newNumber.trim()
 
         // prevent empty submissions
         if (trimmedName === '') {
@@ -28,11 +32,13 @@ const App = () => {
         }
 
         const personObject = {
-            name: trimmedName
+            name: trimmedName,
+            number: trimmedNumber
         }
 
         setPersons(persons.concat(personObject))
         setNewName('') // clear input field
+        setNewNumber('')
 
         console.log('button clicked !!!', event.target)
     }
@@ -44,12 +50,21 @@ const App = () => {
         setNewName(event.target.value)
     }
 
+    const handleNumberChange = (event) => {
+        console.log(event.target.value)
+        setNewNumber(event.target.value)
+    }
+
     return (
         <div>
             <h2>Phonebook</h2>
-            <form onSubmit={addName}>
+            <form onSubmit={addEntry}>
                 <div>
                     name: <input value={newName} onChange={handleNameChange} />
+                </div>
+                <div>
+                    number: <input value={newNumber}
+                    onChange={handleNumberChange}/>
                 </div>
                 <div>
                     <button type="submit">add</button>
