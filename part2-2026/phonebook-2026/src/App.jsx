a6d59107e9f1c131630ec20115c169a346736dcd
cherylfong <42821602+cherylfong@users.noreply.github.com>
2026-08-06 13:25:45 +0000

    complete exercise 2.9
    Added filter field to filter displayed person-number entries.
    Handles typing in the filter field.
    Filtered results are case insensitive.

--- part2-2026/phonebook-2026/src/App.jsx
+++ part2-2026/phonebook-2026/src/App.jsx
@@ -5,13 +5,17 @@ import Person from './components/Person'
 const App = () => {
 
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-1234567'
-         }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
     ])
 
     const [newName, setNewName] = useState('')
 
     const [newNumber, setNewNumber] = useState('')
+
+    const [newFilter, setNewFilter] = useState('')
 
     const addEntry = (event) => {
         event.preventDefault() // prevents form submission
@@ -55,9 +59,23 @@ const App = () => {
         setNewNumber(event.target.value)
     }
 
+    const handleFilterChange = (event) => {
+        console.log(event.target.value)
+        setNewFilter(event.target.value)
+    }
+
+    const filterText = newFilter.trim().toLowerCase()
+    const personsToShow = persons.filter( p =>
+        p.name.toLowerCase().includes(filterText)
+    )
+
     return (
         <div>
-            <h2>Phonebook</h2>
+            <h1>Phonebook</h1>
+            <div>
+                filter shown with <input value={newFilter} onChange={handleFilterChange}/>
+            </div>
+            <h2>add a new</h2>
             <form onSubmit={addEntry}>
                 <div>
                     name: <input value={newName} onChange={handleNameChange} />
@@ -72,7 +90,7 @@ const App = () => {
             </form>
             <h2>Numbers</h2>
             <ul>
-                {persons.map(person =>
+                {personsToShow.map(person =>
                     <Person key={person.id} person={person} />
                 )}
             </ul>
