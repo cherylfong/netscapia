da8eb1d499831b92729cff4e183d6f96258934b4
cherylfong <42821602+cherylfong@users.noreply.github.com>
2026-08-09 11:53:18 +0000

    update code indentation

--- part2-2026/notes-2026/src/components/Footer.jsx
+++ part2-2026/notes-2026/src/components/Footer.jsx
@@ -1,25 +1,25 @@
 const Footer = () => {
 
 
-// Inline styles and some of the other ways of adding styles to React components go completely against the grain of old conventions. Traditionally, it has been considered best practice to entirely separate CSS from the content (HTML) and functionality (JavaScript). According to this older school of thought, the goal was to write CSS, HTML, and JavaScript into their separate files.
+    // Inline styles and some of the other ways of adding styles to React components go completely against the grain of old conventions. Traditionally, it has been considered best practice to entirely separate CSS from the content (HTML) and functionality (JavaScript). According to this older school of thought, the goal was to write CSS, HTML, and JavaScript into their separate files.
 
-// The philosophy of React is, in fact, the polar opposite of this. Since the separation of CSS, HTML, and JavaScript into separate files did not seem to scale well in larger applications, React bases the division of the application along the lines of its logical functional entities.
+    // The philosophy of React is, in fact, the polar opposite of this. Since the separation of CSS, HTML, and JavaScript into separate files did not seem to scale well in larger applications, React bases the division of the application along the lines of its logical functional entities.
 
-// The structural units that make up the application's functional entities are React components. A React component defines the HTML for structuring the content, the JavaScript functions for determining functionality, and also the component's styling; all in one place. This is to create individual components that are as independent and reusable as possible.
+    // The structural units that make up the application's functional entities are React components. A React component defines the HTML for structuring the content, the JavaScript functions for determining functionality, and also the component's styling; all in one place. This is to create individual components that are as independent and reusable as possible.
 
-  const footerStyle = {
-    color: 'green',
-    fontStyle: 'italic'
-  }
+    const footerStyle = {
+        color: 'green',
+        fontStyle: 'italic'
+    }
 
-  return (
-    <div style={footerStyle}>
-      <br />
-      <p>
-        NOTE APP
-      </p>
-    </div>
-  )
+    return (
+        <div style={footerStyle}>
+            <br />
+            <p>
+                NOTE APP
+            </p>
+        </div>
+    )
 }
 
 export default Footer
