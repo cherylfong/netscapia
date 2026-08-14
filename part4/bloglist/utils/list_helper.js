2188179f69e020748b59522e99ba6dcaf7c9fa55
cherylfong <42821602+cherylfong@users.noreply.github.com>
2026-08-14 09:29:56 +0000

    complete exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,17 @@
+// array of blogs as param
 const dummy = (blogs) => {
+  console.log('BLOGS array is', blogs)
   return 1
 }
 
+// array of blogs as param
+// returns sum of likes in list of blogs
+const totalLikes = (blogs) => {
+
+  return blogs.reduce((sum, b) => sum + (b.likes || 0), 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
