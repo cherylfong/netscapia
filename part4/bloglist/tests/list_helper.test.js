7434a2b67f11f5eeb6a452959e8503a27e1846f8
cherylfong <42821602+cherylfong@users.noreply.github.com>
2026-08-14 11:14:35 +0000

    complete exercise 4.6

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -118,6 +118,42 @@ const blogsTwoHighestLikes = [
   }
 ]
 
+
+const blogsTwoMostAuthored = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0
+  },
+  {
+    _id: '5a422ba71b54a676234d17fb',
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0,
+    __v: 0
+  },
+  {
+    _id: '5a422bc61b54a676234d17fc',
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+    __v: 0
+  }
+]
+
 describe('TESTING: totalLikes function', () => {
 
   test('of empty list is zero', () => {
@@ -186,3 +222,45 @@ describe('TESTING: favoriteBlog function', () => {
   })
 
 }) // favoriteBlog test
+
+
+describe('TESTING: mostBlogs function', () => {
+
+  test('of empty list is zero', () => {
+    const result = listHelper.mostBlogs([])
+    assert.deepStrictEqual(result, {})
+  })
+
+  test('when list has only one blog, return 1 author and blog count 1', () => {
+    const result = listHelper.mostBlogs(listWithOneBlog)
+    assert.deepStrictEqual(result, [{
+      author: 'Edsger W. Dijkstra',
+      blogs: 1
+    }])
+  })
+
+  test('when list has more than one blog, return the author with the highest number of blogs ', () => {
+    const result = listHelper.mostBlogs(blogs)
+    assert.deepStrictEqual(result,
+      [{
+        author: 'Robert C. Martin',
+        blogs: 3
+      }])
+  })
+
+  test('when list has more than one blog with at least two authors who has the same highest number of blogs', () => {
+
+    const answer = [  {
+      author: 'Edsger W. Dijkstra',
+      blogs: 2
+    },
+    {
+      author: 'Robert C. Martin',
+      blogs: 2
+    }]
+
+    const result = listHelper.mostBlogs(blogsTwoMostAuthored)
+    assert.deepStrictEqual(result,answer)
+  })
+
+})
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+const lodash = require('lodash')
+
 // array of blogs as param
 const dummy = (blogs) => {
   console.log('BLOGS array is', blogs)
@@ -36,8 +38,43 @@ const favoriteBlog = (blogs) => {
   return result
 }
 
+// array of blogs as param
+// returns the author who has the highest number of blogs
+// return value also contains the number of blogs by the author
+// for example
+// {
+//   author: "Robert C. Martin",
+//   blogs: 3
+// }
+const mostBlogs = (blogs) => {
+  if (!blogs || blogs.length === 0) return {}
+  if (blogs.length === 1) return [{ author: blogs[0].author, blogs: 1 }]
+
+  const result = []
+
+  for (const b of blogs) {
+    const author = b.author
+    // find if author already exists in results
+    const entry = lodash.find(result, { author })
+    if (!entry) {
+      result.push({ author, blogs: 1 })
+    } else {
+      entry.blogs += 1
+    }
+  }
+
+  // return the author object with the highest blog count
+  const top = lodash.maxBy(result, 'blogs')
+
+  // check to see if there are coliding (more than one highest number of the same value)
+  const winners = result.filter(r => r.blogs === top.blogs)
+
+  return winners
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
