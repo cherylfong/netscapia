fcc7266365f741fbd1d776eefe4003ac3318382f
cherylfong <42821602+cherylfong@users.noreply.github.com>
2026-08-20 15:34:07 +0000

    add starting files of notes app frontend for part 5 sub a

---
+++ part5/notes-frontend/src/main.jsx
@@ -0,0 +1,31 @@
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+import './index.css'
+
+// content below is moved into db.json
+// fetching data from server is done in App.jsx
+//
+// const notes = [
+//   {
+//     id: 1,
+//     content: 'HTML is easy',
+//     important: true
+//   },
+//   {
+//     id: 2,
+//     content: 'Browser can execute only JavaScript',
+//     important: false
+//   },
+//   {
+//     id: 3,
+//     content: 'GET and POST are the most important methods of HTTP protocol',
+//     important: true
+//   }
+// ]
+//
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App/>
+)
