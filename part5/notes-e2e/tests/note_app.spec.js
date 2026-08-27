8460e8ef2758282ef835c514d3d052dac92e34cd
cherylfong <42821602+cherylfong@users.noreply.github.com>
2026-08-27 16:13:10 +0000

    add tests to check user login and note creation from part 5 sub d "Test initialization" and "Testing note Creation"

--- part5/notes-e2e/tests/note_app.spec.js
+++ part5/notes-e2e/tests/note_app.spec.js
@@ -1,13 +1,61 @@
-const { describe, test, expect } = require('@playwright/test')
+const { describe, test, expect, beforeEach } = require('@playwright/test')
 
 describe('Note app', () => {
 
-    test('front page can be opened', async ({ page }) => {
+    beforeEach(async ({ page }) => {
         await page.goto('http://localhost:5173')
+    })
+
+    test('front page can be opened', async ({ page }) => {
 
         const locator = page.getByText('Notes')
         await expect(locator).toBeVisible()
         await expect(page.getByText('NOTE APP')).toBeVisible()
+    })
+
+    test('user can log in', async ({ page }) => {
+
+        await page.getByRole('button', { name: 'Welcome! Want to Login?' }).click()
+
+        // use first() and last() when there are more than 1 textbox
+        //
+        // await page.getByRole('textbox').first().fill('jelly')
+        // await page.getByRole('textbox').last().fill('password')
+
+        // const textboxes = await page.getByRole('textbox').all()
+        // await textboxes[0].fill('jelly')
+        // await textboxes[1].fill('password')
+        // if the registration form is changed, the tests may break, 
+        // as they rely on the fields to be on the page in a certain order
+        //
+        // option to use:
+        // https://playwright.dev/docs/api/class-page#page-get-by-test-id
+
+
+        await page.getByLabel('username').fill('jelly')
+        await page.getByLabel('password').fill('password')
+
+        await page.getByRole('button', { name: 'login' }).click()
+
+        await expect(page.getByText('jelly is logged in.')).toBeVisible()
+
+    })
+
+    describe('when logged in', () => {
+        beforeEach(async ({ page }) => {
+            await page.getByRole('button', { name: 'Welcome! Want to Login?' }).click()
+            await page.getByLabel('username').fill('jelly')
+            await page.getByLabel('password').fill('password')
+
+            await page.getByRole('button', { name: 'login' }).click()
+        })
+
+        test('a new note can be created', async ({ page }) => {
+            await page.getByRole('button', { name: 'Create a new note?' }).click()
+            await page.getByLabel('content').fill('a note created by playwright')
+            await page.getByRole('button', { name: 'save' }).click()
+            await expect(page.getByText('a note created by playwright')).toBeVisible()
+        })
     })
 
 })
